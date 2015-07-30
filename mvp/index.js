498fe3072b04cc58d6abeb00f02288a5a3bc2f20
eladrk <eladkt@gmail.com>
2015-07-30 16:41:26 +0300

    added ;

--- mvp/index.js
+++ mvp/index.js
@@ -1,9 +1,9 @@
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
-app.listen(3000)
+app.listen(3000);
