9ba5fd6b7bcef0b2a53b9742634b5aa1a6a7b96a
Elad Katz <ekatz@localhost.localdomain>
2015-08-07 17:56:25 +0300

    added basic api

--- mvp/index.js
+++ mvp/index.js
@@ -1,9 +1,29 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+
+//app.use(express.bodyParser());
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.send('Hello World');
+  
+  console.log('get message');
 });
+
+app.get('/pushAlert', function (req, res) {
+  res.send('alert push api');
+  
+  console.log('got message');
+});
+
+app.post('/push2', function(req, res) {
+  res.send(req.body.title);
+  
+  console.log("ack: " + req.body.title);
+  
+});
+
 
 app.listen(3000);
