399756bedb78bb5210ac04fe644b313f3e656c6c
eladrk <eladkt@gmail.com>
2015-08-16 14:33:38 +0300

    fixes for crawler. got stuck on BOT like behavior

--- mvp/crawler.js
+++ mvp/crawler.js
@@ -26,14 +26,14 @@ var options = {
 // };
 
 var req = http.request(options, function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  // console.log('STATUS: ' + res.statusCode);
+  // console.log('HEADERS: ' + JSON.stringify(res.headers));
   res.setEncoding('utf8');
   res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
+    console.log(chunk);
   });
   
-  console.log('-------------');
+  // console.log('-------------');
   
 });
 
@@ -41,7 +41,7 @@ req.on('error', function(e) {
   console.log('problem with request: ' + e.message);
 });
 
-// write data to request body
-req.write('data\n');
-req.write('data\n');
+// // write data to request body
+// req.write('data\n');
+// req.write('data\n');
 req.end();
